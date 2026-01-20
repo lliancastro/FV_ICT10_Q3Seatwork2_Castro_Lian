b7a6d0e501fb0fc16831cd2f878e56b3484675f4
lliancastro <lianc4793@gmail.com>
2026-01-20 08:57:26 +0800

    Add files via upload

---
+++ 23.py
@@ -0,0 +1,27 @@
+from pyscript import display 
+
+Nested Conditional Statements
+Complex decision making 
+
+emerald = float(input('Enter a number'))
+ruby = float(input('Enter a number'))
+sapphire = float(input('Enter a number'))
+topaz = float(input('Enter a number'))
+jade = float(input('Enter a number'))
+
+#Check if eligible 
+if emerald has registered :
+    if ruby has registered : 
+        print("Congrats you're part of Red bulldogs!")
+    else:
+        print("Congrats you're part of Green hornets!")
+    else:
+        print('Please double check and get your clearance')
+
+if sapphire has registered :
+    if topaz has registered : 
+        print("Congrats you're part of Blue Bears!")
+    else:
+        print("Congrats you're part of Yellow Tigers!")
+    else:
+        print('Please double check and get your clearance')
